cd964b96d82f986b4c2c7ad0d1e61b807090dad5
sep <cryzje2@gmail.com>
2023-10-10 15:12:23 +0300

    rewrote funcs + included retrieve location functionality

--- main.py
+++ main.py
@@ -4,17 +4,25 @@ url = input('input url: ')
 sitemapLinks = extractSitemapLinks(url)
 sitemapUrl = ''
 newsTagFlag = False
+lastmodFlag = False
 if checkIfValidRobots(url):
     for i in sitemapLinks[:]:
-        if len(sitemapLinks) > 0:
+        if newsTagFlag and lastmodFlag:
+            break
+        elif len(sitemapLinks) > 0:
             sitemapUrl = sitemapLinks[0]
-            if newsTagExists(sitemapUrl):
-                newsTagFlag = True
-            sitemapLinks.pop(0)
+            if publicationDateExists(sitemapUrl):
+                lastmodFlag = True
+                if newsTagExists(sitemapUrl):
+                    newsTagFlag = True
+                sitemapLinks.pop(0)
         else:
             break
-elif checkSitemapXml(url):
-    sitemapUrl = url + '/sitemap.xml'
-    if newsTagExists(sitemapUrl):
-        newsTagFlag = True
-print(newsTagFlag)
+
+if newsTagFlag and lastmodFlag:
+    print(f'<news:news> and lastmod / publication date is available for at least one sitemap, url: {sitemapUrl}')
+    loc = retrieveLocation(sitemapUrl)
+    print(loc)
+else:
+    print('Not valid')
+
