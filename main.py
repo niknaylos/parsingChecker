a092cf1b49ce0c9638f811117645e61405b62bb1
sep <cryzje2@gmail.com>
2023-10-12 12:33:45 +0300

    datechecks fixes+

--- main.py
+++ main.py
@@ -15,12 +15,12 @@ if checkIfValidRobots(url):
         if publicationDate and (validDate or validDate is None):
             locTag = processSitemap(sitemapUrl)
             if locTag:
-                print(f'Valid publication date and loc tag found for sitemap {i + 1}, url: {sitemapUrl}')
+                print(f'Valid publication date {publicationDate} and loc tag found for sitemap {i + 1}, url: {sitemapUrl}')
                 print(f'Loc Tag: {locTag}')
                 print(f'News Tag Present: {newsTagFlag}')
                 break  # Exit the loop if a valid locTag is found
-        else:
-            print(f'Publication date in sitemap {i + 1} is not valid or missing.')
+        # else:
+        #     print(f'Publication date in sitemap {i + 1} is too old, not valid or missing.')
             """todo : only check if date could be incorrect and notify instead of stopping script"""
 else:
     print('Not valid due to missing or invalid robots.txt')
